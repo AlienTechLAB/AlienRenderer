d54de82f632f4a4e92054e2a267acb55c1abd226
Sebastian <contact@alientechlab.com>
2017-02-25 14:00:50 +0100

    eVoWindowsFrameBuffer

--- WinMain.cpp
+++ WinMain.cpp
@@ -8,25 +8,24 @@
 //---------------------------------------------------------------------------------------------------------
 
 HWND WindowHandle;
-HDC  DeviceContext;
-HDC  MemoryContext;
-HBITMAP Bitmap;
-HGDIOBJ OldBitmap;
 
 #define WIDTH 1024
 #define HEIGHT 784
 
-int BufferSize = 0;
-unsigned int* Buffer = NULL;
-
 int pixel = 0;
+
+eVoWindowsFrameBuffer FrameBuffer;
+
 //---------------------------------------------------------------------------------------------------------
 
 void Render()
 {
-	for (int i = 0; i < BufferSize / 4; i++)
+	int pixelsNo = FrameBuffer.GetBufferSizeInBytes() / 4;
+	unsigned int* pBuffer = (unsigned int*)FrameBuffer.GetBuffer();
+
+	for (int i = 0; i < pixelsNo; i++)
 	{
-		((unsigned int*)Buffer)[i] = pixel++;
+		pBuffer[i] = pixel++;
 	}
 }
 
@@ -38,6 +37,7 @@ LONG WINAPI WindowProc(HWND windowHandle, UINT uMsg, WPARAM wParam, LPARAM lPara
 	long diff;
 	float frameTime;
 	float fps;
+	HDC deviceContext;
 
 	switch (uMsg)
 	{
@@ -53,8 +53,9 @@ LONG WINAPI WindowProc(HWND windowHandle, UINT uMsg, WPARAM wParam, LPARAM lPara
 			fps = 1.0f / frameTime;
 			std::cout << fps << std::endl;
 
-
-			BitBlt(DeviceContext, 0, 0, WIDTH, HEIGHT, MemoryContext, 0, 0, SRCCOPY);
+			deviceContext = GetDC(windowHandle);
+			BitBlt(deviceContext, 0, 0, FrameBuffer.GetWidth(), FrameBuffer.GetHeight(), FrameBuffer.GetMemoryContext(), 0, 0, SRCCOPY);
+			ReleaseDC(windowHandle, deviceContext);
 			EndPaint(windowHandle, &ps);
 			return 0;
 
@@ -186,26 +187,7 @@ int APIENTRY WinMain(HINSTANCE hCurrentInst, HINSTANCE hPreviousInst, LPSTR lpsz
 		{
 			if (DefinePixelFormat())
 			{
-				DeviceContext = GetDC(WindowHandle);
-				MemoryContext = CreateCompatibleDC(DeviceContext);
-
-				BITMAPINFO bmpInfo;
-				bmpInfo.bmiHeader.biSize = sizeof(bmpInfo);
-				bmpInfo.bmiHeader.biWidth = WIDTH;
-				bmpInfo.bmiHeader.biHeight = HEIGHT;
-				bmpInfo.bmiHeader.biPlanes = 1;
-				bmpInfo.bmiHeader.biBitCount = 32;
-				bmpInfo.bmiHeader.biCompression = BI_RGB;
-				bmpInfo.bmiHeader.biSizeImage = ((WIDTH * (bmpInfo.bmiHeader.biBitCount / 8) + 3) & -4) * HEIGHT;
-
-				void* pBuffer = NULL;
-				Bitmap = CreateDIBSection(MemoryContext, &bmpInfo, DIB_RGB_COLORS, &pBuffer, NULL, 0);
-				
-				BufferSize = bmpInfo.bmiHeader.biSizeImage;
-				Buffer = (unsigned int*)pBuffer;
-
-				//Bitmap = CreateCompatibleBitmap(MemoryContext, WIDTH, HEIGHT);
-				OldBitmap = SelectObject(MemoryContext, Bitmap);
+				FrameBuffer.Init(WIDTH, HEIGHT, WindowHandle);
 
 				// Main loop
 				while (true)
@@ -220,14 +202,11 @@ int APIENTRY WinMain(HINSTANCE hCurrentInst, HINSTANCE hPreviousInst, LPSTR lpsz
 					if (msg.message == WM_QUIT)
 						break;
 
-
 					InvalidateRect(WindowHandle, NULL, false);
 				}
 
 				// Release
-				SelectObject(MemoryContext, OldBitmap);
-				DeleteObject(Bitmap);
-				DeleteDC(MemoryContext);
+				FrameBuffer.Release();
 				DestroyWindow(WindowHandle);
 			}
 		}
