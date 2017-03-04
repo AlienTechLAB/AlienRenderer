39571a91b2fd7839f0f7331ae9bb1c5c6fb8e872
Sebastian <contact@alientechlab.com>
2017-03-04 09:49:30 +0100

    eVoMatrix4x4

--- Application/Application.hpp
+++ Application/Application.hpp
@@ -10,7 +10,7 @@ class Application
 	private:
 	eVoFrameBuffer<eVoColor32>* FrameBuffer = NULL;
 	eVoRenderer* Rednerer = NULL;
-	eVoVector3* Vertices = NULL;
+	eVoVector3*  Vertices = NULL;
 
 	//---------------------------------------------------------------------------------------------------------
 
