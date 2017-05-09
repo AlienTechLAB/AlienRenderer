4131545664758a6c61b3f35d35fb7c7c99bb3630
Alien TechLAB <contact@alientechlab.com>
2017-05-09 21:20:20 +0200

    eVoPoint3

---
+++ Alien Renderer/eVolution3D/eVoPoint3.hpp
@@ -0,0 +1,109 @@
+#pragma once
+#include <math.h> 
+#include "eVoException.hpp"
+#include "eVoMath.hpp"
+#include "eVoEnums.h"
+#include "eVoVector3.hpp"
+
+struct eVoPoint3
+{
+	//---------------------------------------------------------------------------------------------------------
+
+	public:
+	float x, y, z;
+
+	//---------------------------------------------------------------------------------------------------------
+
+	public: eVoPoint3()
+	{
+		x = y = z = 0;
+	}
+
+	//---------------------------------------------------------------------------------------------------------
+
+	public: eVoPoint3(float ax, float ay, float az)
+	{
+		x = ax;
+		y = ay;
+		z = az;
+	}
+
+	//---------------------------------------------------------------------------------------------------------
+
+	public: eVoPoint3 operator=(eVoPoint3& point)
+	{
+		x = point.x;
+		y = point.y;
+		z = point.z;
+		return *this;
+	}
+
+	//---------------------------------------------------------------------------------------------------------
+
+	public: eVoVector3 operator-(eVoPoint3& point)
+	{
+		return eVoVector3(x - point.x, y - point.y, z - point.z);
+	}
+
+	//---------------------------------------------------------------------------------------------------------
+
+	public: bool operator==(eVoPoint3& point)
+	{
+		if (x != point.x) return false;
+		else if (y != point.y) return false;
+		else if (z != point.z) return false;
+		else return true;
+	}
+
+	//---------------------------------------------------------------------------------------------------------
+
+	public: bool operator!=(eVoPoint3& point)
+	{
+		if (x != point.x) return true;
+		else if (y != point.y) return true;
+		else if (z != point.z) return true;
+		else return false;
+	}
+
+	//---------------------------------------------------------------------------------------------------------
+
+	public: void Rotate(float angle, eVoAxis axis)
+	{
+		float c;
+
+		switch (axis)
+		{
+		case eVoAxis::X:
+			c = y;
+			y = y * cos(angle) - z * sin(angle);
+			z = c * sin(angle) + z * cos(angle);
+			break;
+
+		case eVoAxis::Y:
+			c = x;
+			x = z * sin(angle) + x * cos(angle);
+			z = z * cos(angle) - c * sin(angle);
+			break;
+
+		case eVoAxis::Z:
+			c = x;
+			x = y * sin(angle) + x * cos(angle);
+			y = y * cos(angle) - c * sin(angle);
+			break;
+
+		default:
+			throw eVoException("Unknown eVoAxis.");
+		};
+	}
+
+	//---------------------------------------------------------------------------------------------------------
+
+	public: void SetCoords(float newX, float newY, float newZ)
+	{
+		x = newX;
+		y = newY;
+		z = newZ;
+	}
+
+	//---------------------------------------------------------------------------------------------------------
+};
